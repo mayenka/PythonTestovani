ce2c0b24273b63c6eab98ded4fe78e5c4a20cefb
Maya Kopecka <maya.kopecka@gmail.com>
2019-05-18 11:39:21 +0200

    Přidána funkce if__name__ == '__main__' a while not is_valid_play(human)

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,29 +10,30 @@ def is_valid_play(play):
 
 
 
-
-human = input('rock, paper or scissors? ')
-
-while human not in ['rock', 'paper', 'scissors']:
+if __name__ == '__main__':      #proměnná == řetezec
     human = input('rock, paper or scissors? ')
 
-computer = random.choice(['rock', 'paper', 'scissors'])
+    while not is_valid_play(human):
+    # while human not in ['rock', 'paper', 'scissors']:
+        human = input('rock, paper or scissors? ')
 
-print(computer)
+    computer = random.choice(['rock', 'paper', 'scissors'])
 
-if human == computer:
-    print('it\'s a tie!')
-elif human == 'rock' and computer == 'scissors':
-    print('you win!')
-elif human == 'rock' and computer == 'paper':
-    print('you loose!')
-elif human == 'scissors' and computer == 'rock':
-    print('you loose!')
-elif human == 'scissors' and computer == 'paper':
-    print('you win!')
-elif human == 'paper' and computer == 'rock':
-    print('you wins!')
-elif human == 'paper' and computer == 'scissors':
-    print('you loose!')
+    print(computer)
+
+    if human == computer:
+        print('it\'s a tie!')
+    elif human == 'rock' and computer == 'scissors':
+        print('you win!')
+    elif human == 'rock' and computer == 'paper':
+        print('you loose!')
+    elif human == 'scissors' and computer == 'rock':
+        print('you loose!')
+    elif human == 'scissors' and computer == 'paper':
+        print('you win!')
+    elif human == 'paper' and computer == 'rock':
+        print('you wins!')
+    elif human == 'paper' and computer == 'scissors':
+        print('you loose!')
 else:
     print('mistake, please play again!')
