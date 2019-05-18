1ce675a4cf86bc406234133d03404800c4f7654f
Maya Kopecka <maya.kopecka@gmail.com>
2019-05-18 11:24:04 +0200

    přidána funkce na validnost tahu

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,16 @@
 import random
 
+def is_valid_play(play):
+    return play in ['rock', 'paper', 'scissors']    #to stejné jako podmínka s if
+
+
+
+
+
+
+
+
+
 human = input('rock, paper or scissors? ')
 
 while human not in ['rock', 'paper', 'scissors']:
@@ -12,16 +23,16 @@ print(computer)
 if human == computer:
     print('it\'s a tie!')
 elif human == 'rock' and computer == 'scissors':
-    print('human wins!')
+    print('you win!')
 elif human == 'rock' and computer == 'paper':
-    print('computer wins!')
+    print('you loose!')
 elif human == 'scissors' and computer == 'rock':
-    print('computer wins!')
+    print('you loose!')
 elif human == 'scissors' and computer == 'paper':
-    print('human wins!')
+    print('you win!')
 elif human == 'paper' and computer == 'rock':
-    print('human wins!')
+    print('you wins!')
 elif human == 'paper' and computer == 'scissors':
-    print('computer wins!')
+    print('you loose!')
 else:
     print('mistake, please play again!')
