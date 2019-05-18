6c0e5f9b27dfb63e3368455b0a4eae328f239a25
Maya Kopecka <maya.kopecka@gmail.com>
2019-05-18 10:53:54 +0200

    Přidána začátečnická funkce na výpočet ellipse area, která je špatně napsaná, protože bere hodnoty z inputu a navíc výsledek jde jen do printu

--- shapes.py
+++ shapes.py
@@ -1,7 +1,14 @@
 from math import pi
 
 
-def ellipse_area(a, b):
-    """Given the lengths of the semi-major and semi-minor axes, returns the
-    area of the ellipse."""
-    return pi * a * b       #návratová hodnota
+# def ellipse_area(a, b):
+#     """Given the lengths of the semi-major and semi-minor axes, returns the
+#     area of the ellipse."""
+#     return pi * a * b       #návratová hodnota
+
+
+#jak napíše funkci začátečník
+def ellipse_area():
+    a = float(input('Zadej mi první poloosu'))
+    b = float(input('Zadej mi druhou poloosu'))
+    print('Obsah elipsy je', a * b * pi)
