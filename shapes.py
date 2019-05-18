2ee11cbe1eb0e9479c6c469968c6eb6c0a8a72a4
Maya Kopecka <maya.kopecka@gmail.com>
2019-05-18 10:49:37 +0200

    Napsána základní funkce na spočítání obsahu elipsy

---
+++ shapes.py
@@ -0,0 +1,7 @@
+from math import pi
+
+
+def ellipse_area(a, b):
+    """Given the lengths of the semi-major and semi-minor axes, returns the
+    area of the ellipse."""
+    return pi * a * b       #návratová hodnota
