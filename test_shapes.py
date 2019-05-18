e7c83eb0d6248f1da62945450906b25da0da3d7f
Maya Kopecka <maya.kopecka@gmail.com>
2019-05-18 10:57:55 +0200

    Přidán špatný test na špatný kód

--- test_shapes.py
+++ test_shapes.py
@@ -1,8 +1,12 @@
 import math
 import shapes
 
-def test_circle_area_is_pi():
-    # area = shapes.ellipse_area(1, 1)
-    # assert area == math.pi
+# def test_circle_area_is_pi():
+#     # area = shapes.ellipse_area(1, 1)
+#     # assert area == math.pi
+#
+#     assert shapes.ellipse_area(1,1) == math.pi     #spočítá plochu kruhu o velikosti 1
 
-    assert shapes.ellipse_area(1,1) == math.pi
+#test na špatný kód
+def test_circle_area_is_pi():
+    shapes.ellipse_area()
